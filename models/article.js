6e23c1670e5c4541da486bce182bf9e844782a77
Uliana <85169486+Ulianamss@users.noreply.github.com>
2021-09-12 22:55:36 +0300

    Add files via upload

--- models/article.js
+++ models/article.js
@@ -1,1 +1,17 @@
+const mongoose = require("mongoose");
+const articleSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    description: String,
+    theme: String,
+    views: {type: Number, default: 0}, 
+    createdAt: { type: Date, default: Date.now },
+    comments: [
+        {
+           type: mongoose.Schema.Types.ObjectId,
+           ref: "comment"
+        }
+     ]
+ });
 
+module.exports = mongoose.model("article", articleSchema);
